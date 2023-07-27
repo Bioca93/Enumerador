5b553ed72545bda9da60be10791c1b43b36d4c9d
Beatriz Ressurreição <bea_jrr_93@hotmail.com>
2023-07-27 19:50:55 +0100

    first deploy

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EnumEx
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Enums
+            string segunda = "Segunda-Feira";
+
+            int diaSemana = (int)DiasSemana.Segunda;
+
+            Console.WriteLine(diaSemana);
+            Console.WriteLine((DiasSemana)4);
+            Console.WriteLine(DiasSemana.Segunda);
+
+            Console.WriteLine(segunda);
+            Console.WriteLine("Segunda-Feira");
+            Console.WriteLine("Segunda-Feira");
+        }
+    }
+
+    public enum DiasSemana
+    {
+        Segunda = 1,
+        TercaFeira = 2, // Terça-Feira
+        QuartaFeira = 3,
+        QuintaFeira = 4,
+        SextaFeira = 5,
+        Sabado = 6,
+        Domingo = 7,
+        DiaXpto
+    }
+}
